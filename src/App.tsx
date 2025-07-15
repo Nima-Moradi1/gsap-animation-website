c2fc7d6f3062471395b664e39b15819f3657b9c7
nima moradi <mnima8100@gmail.com>
2025-07-15 20:40:28 +0330

    fix : added a loader before images for preloading

--- src/App.tsx
+++ src/App.tsx
@@ -7,14 +7,41 @@ import About from "./components/About";
 import Art from "./components/Art";
 import Menu from "./components/Menu";
 import Contact from "./components/Contact";
+import { useEffect, useState } from "react";
+import Loader from "./components/Loader";
 
 // DO NOT FORGET to register the plugins before using them
 gsap.registerPlugin(ScrollTrigger, SplitText);
 
 function App() {
 
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    // Wait for fonts and assets to be ready (simulate with setTimeout)
+    const waitForFonts = async () => {
+    await document.fonts.ready;
+    }
+    const timeout = setTimeout(() => {
+      setIsLoading(false);
+
+      // Trigger entrance animations here
+      gsap.fromTo(
+        "#page-content",
+        { opacity: 0, y: 30 },
+        { opacity: 1, y: 0, duration: 1 }
+      );
+      waitForFonts()
+    }, 5000);
+
+    return () => clearTimeout(timeout);
+  }, []);
 
   return (
+    <>
+    {
+      isLoading ? <Loader />  :
+    
     <main className="overflow-hidden">
       <Navbar />
       <Hero />
@@ -24,6 +51,8 @@ function App() {
       <Menu />
       <Contact />
     </main>
+    }
+  </>
   )
 }
 
---
+++ src/components/Loader.tsx
@@ -0,0 +1,8 @@
+
+export default function Loader() {
+  return (
+    <div className="fixed inset-0 z-50 flex items-center justify-center bg-black text-white">
+      <div className="animate-spin rounded-full h-16 w-16 border-b-4 border-white" />
+    </div>
+  );
+}
